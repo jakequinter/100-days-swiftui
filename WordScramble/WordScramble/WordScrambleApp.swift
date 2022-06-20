4abca12bdbdeaed3ec5ae5ec9be5b231176fa8a8
Jake Quinter <hello@jakequinter.io>
2022-06-20 15:06:12 -0400

    word scramble

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jake Quinter on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
