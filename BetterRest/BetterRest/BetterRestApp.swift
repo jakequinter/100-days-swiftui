bc378d75a803c91f1da0ea6bdad4ccd13a7aebb1
Jake Quinter <hello@jakequinter.io>
2022-06-20 12:12:04 -0400

    better rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jake Quinter on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
