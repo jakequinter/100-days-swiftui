744f50d48914a0893906b1e327947727702546fd
Jake Quinter <hello@jakequinter.io>
2022-07-02 12:29:05 -0400

    project 8

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Jake Quinter on 7/2/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
